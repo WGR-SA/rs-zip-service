52ad8530a8168edbab31f352d2fc60cd601e2dd7
jeanvier <damien.grossfeld@gmail.com>
2023-11-14 17:23:30 +0100

    feat: zip

--- src/main.rs
+++ src/main.rs
@@ -2,11 +2,54 @@ pub mod config;
 pub mod storage;
 pub mod zip_stream;
 
+use crate::zip_stream::ZipStream;
 use actix_web::{post, web, App, Error, HttpRequest, HttpResponse, HttpServer};
+use async_zip::base::write::ZipFileWriter;
+use async_zip::Compression;
+use async_zip::ZipEntryBuilder;
+use async_zip::ZipString;
 use dotenv::dotenv;
-use futures::{future::ok, stream::once};
+use futures::stream::StreamExt;
+use futures::{io::AsyncWrite, io::AsyncWriteExt, stream::TryStreamExt};
+use std::pin::Pin;
+use std::task::{Context, Poll};
 use tokio::sync::mpsc;
-use zip_stream::ZipStream;
+// use zip_stream::ZipStream;
+
+pub struct CustomWriter {
+    sender: mpsc::Sender<bytes::Bytes>,
+}
+
+impl CustomWriter {
+    // Fields to hold state and possibly communicate with the response stream
+    pub fn new(o_sender: mpsc::Sender<bytes::Bytes>) -> CustomWriter {
+        CustomWriter { sender: o_sender }
+    }
+
+    pub fn write_all_buf(&mut self, buf: &mut bytes::Bytes) {
+        self.sender.try_send(buf.clone()).unwrap();
+    }
+}
+
+impl AsyncWrite for CustomWriter {
+    fn poll_write(
+        self: Pin<&mut Self>,
+        cx: &mut Context<'_>,
+        buf: &[u8],
+    ) -> Poll<Result<usize, std::io::Error>> {
+        println!("poll_write {:?}", buf.len());
+        //self.sender.try_send(bytes::Bytes::from(buf)).unwrap();
+        Poll::Ready(Ok(buf.len()))
+    }
+
+    fn poll_flush(self: Pin<&mut Self>, cx: &mut Context<'_>) -> Poll<Result<(), std::io::Error>> {
+        Poll::Ready(Ok(()))
+    }
+
+    fn poll_close(self: Pin<&mut Self>, cx: &mut Context<'_>) -> Poll<Result<(), std::io::Error>> {
+        Poll::Ready(Ok(()))
+    }
+}
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
@@ -27,18 +70,34 @@ async fn get_files_stream_zip(req: HttpRequest, body: web::Json<Vec<String>>) ->
     if !storage_instance.connect().await {
         return HttpResponse::InternalServerError().body("Error connecting to storage");
     }
-    let (sender, receiver) = mpsc::channel(32);
-    let (sender_test, mut receiver_test) = mpsc::channel(32);
-    let stream = ZipStream::new(receiver);
+    let (i_sender, mut i_receiver) = mpsc::channel(32);
+    let (o_sender, mut o_receiver) = mpsc::channel(32);
+
+    let mut writer = CustomWriter::new(o_sender);
+    let mut zip = ZipFileWriter::new(&mut writer);
 
     for path in body.into_inner() {
-        println!("Sending file: {}", path);
-        let _s = storage_instance.send_file_stream(&sender_test, path).await;
+        let _is = storage_instance.send_file_stream(&i_sender, path).await;
     }
 
-    while let Some(file_stream) = receiver_test.recv().await {
-        println!("Receving file: {}", file_stream.name);
+    while let Some(file_stream) = i_receiver.recv().await {
+        println!("Sending {:?} to zip", file_stream.name);
+        let mut stream = file_stream.stream;
+        let entry = ZipEntryBuilder::new(ZipString::from(file_stream.name), Compression::Deflate);
+        let mut entry_writer = zip.write_entry_stream(entry).await.unwrap();
+        while let Some(Ok(chunk)) = stream.next().await {
+            entry_writer.write_all(&chunk).await.unwrap();
+        }
+        entry_writer.close().await.unwrap();
+        println!("Sended");
     }
+
+    writer.close().await.unwrap();
+    println!("Finished sending files to zip stream");
+
+    //stream.process_files();
+    //let response_stream = o_receiver.map(Ok::<_, Error>);
+    let response_stream = ZipStream::new();
 
     HttpResponse::Ok()
         .content_type("application/zip")
@@ -46,5 +105,5 @@ async fn get_files_stream_zip(req: HttpRequest, body: web::Json<Vec<String>>) ->
             "ContentDisposition",
             "attachment; filename=\"download.zip\"",
         ))
-        .streaming(stream)
+        .streaming(response_stream)
 }
--- src/zip_stream/mod.rs
+++ src/zip_stream/mod.rs
@@ -1,56 +1,23 @@
-use crate::storage::FileStream;
 use actix_web::Error;
 use futures::{
     stream::Stream,
     task::{Context, Poll},
-    StreamExt,
 };
-use std::io::Cursor;
-use std::io::Write;
 use std::pin::Pin;
-use tokio::sync::mpsc;
-use zip::{write::FileOptions, ZipWriter};
-
-// Assume FileStream is defined elsewhere
 
 pub struct ZipStream {
-    zip_writer: Option<ZipWriter<Cursor<Vec<u8>>>>,
-    receiver: mpsc::Receiver<FileStream>,
-    buffer: Option<bytes::Bytes>, // Buffer to store the final ZIP data
+    buffer: Option<bytes::Bytes>,
 }
 
 impl ZipStream {
-    pub fn new(receiver: mpsc::Receiver<FileStream>) -> ZipStream {
-        println!("new zip");
-        ZipStream {
-            zip_writer: Some(ZipWriter::new(Cursor::new(Vec::new()))),
-            receiver,
-            buffer: None,
-        }
+    pub fn new() -> ZipStream {
+        println!("new zip stream");
+        ZipStream { buffer: None }
     }
 
-    // Asynchronous function to process files and create ZIP
-    pub async fn process_files(&mut self) {
-        while let Some(file_stream) = self.receiver.recv().await {
-            println!("Processing file: {:?}", file_stream.name);
-            if let Some(zip_writer) = self.zip_writer.as_mut() {
-                zip_writer
-                    .start_file(&file_stream.name, FileOptions::default())
-                    .unwrap();
-                let mut stream = file_stream.stream;
-
-                while let Some(Ok(chunk)) = stream.next().await {
-                    zip_writer.write_all(&chunk).unwrap();
-                }
-            }
-        }
-
-        if let Some(_zip_writer) = self.zip_writer.take() {
-            let mut zip_writer = self.zip_writer.take().expect("Zip writer not found");
-            let cursor = zip_writer.finish().expect("Failed to finish ZIP file");
-            let data = cursor.into_inner();
-            self.buffer = Some(bytes::Bytes::from(data));
-        }
+    pub fn append_buffer(&mut self, buffer: bytes::Bytes) {
+        println!("append_buffer");
+        self.buffer = Some(buffer);
     }
 }
 
@@ -58,9 +25,12 @@ impl Stream for ZipStream {
     type Item = Result<bytes::Bytes, Error>;
 
     fn poll_next(mut self: Pin<&mut Self>, cx: &mut Context<'_>) -> Poll<Option<Self::Item>> {
+        println!("poll_next");
         if let Some(buffer) = self.buffer.take() {
+            println!("Buffer len: {:?}", buffer.len());
             Poll::Ready(Some(Ok(buffer)))
         } else {
+            println!("Empty Buffer");
             cx.waker().wake_by_ref();
             Poll::Pending
         }
